aec5fc108fd4f376119c7dbfd9a6be8717989995
sohanPaycraft <sohan.sagar@paycraftsol.com>
2023-02-08 16:45:08 +0530

    jj

---
+++ tests/conftest.py
@@ -0,0 +1,60 @@
+import pytest
+from selenium import webdriver
+import time
+
+from webdriver_manager.chrome import ChromeDriverManager
+
+driver = None
+
+def pytest_addoption(parser):
+    parser.addoption(
+        "--browser_name", action="store", default="chrome"
+    )
+
+
+@pytest.fixture(scope="class")
+def setup(request):
+    global driver
+    browser_name=request.config.getoption("browser_name")
+    options = webdriver.ChromeOptions()
+    options.add_argument('--allow-insecure-localhost')  # differ on driver version. can ignore.
+    caps = options.to_capabilities()
+    caps["acceptInsecureCerts"] = True
+
+    driver = webdriver.Chrome(ChromeDriverManager().install(), desired_capabilities=caps)
+    driver.maximize_window()
+    driver.get("https://rahulshettyacademy.com/angularpractice/")
+    driver.maximize_window()
+
+    request.cls.driver = driver
+    yield
+    driver.close()
+
+
+@pytest.mark.hookwrapper
+def pytest_runtest_makereport(item):
+    """
+        Extends the PyTest Plugin to take and embed screenshot in html report, whenever test fails.
+        :param item:
+        """
+    pytest_html = item.config.pluginmanager.getplugin('html')
+    outcome = yield
+    report = outcome.get_result()
+    extra = getattr(report, 'extra', [])
+
+    if report.when == 'call' or report.when == "setup":
+        xfail = hasattr(report, 'wasxfail')
+        if (report.skipped and xfail) or (report.failed and not xfail):
+            file_name = report.nodeid.replace("::", "_") + ".png"
+            _capture_screenshot(file_name)
+            if file_name:
+                html = '<div><img src="%s" alt="screenshot" style="width:304px;height:228px;" ' \
+                       'onclick="window.open(this.src)" align="right"/></div>' % file_name
+                extra.append(pytest_html.extras.html(html))
+        report.extra = extra
+
+
+def _capture_screenshot(name):
+        yield
+        driver.get_screenshot_as_file(name)
+
